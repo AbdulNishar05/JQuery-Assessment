56b747b1c3b5d1b68e568be6ef4fe4bb84479ddd
AbdulNishar05 <80945971+AbdulNishar05@users.noreply.github.com>
2021-04-15 17:56:10 +0530

    Update bank.js

--- BankCreditDebit Assessment/bank.js
+++ BankCreditDebit Assessment/bank.js
@@ -2,14 +2,17 @@ $(document).ready(function () {
      totalBalance =1000;
     const userDeposit = $("#userDeposit");
     
-    
     var arr = JSON.parse(localStorage.getItem("userBalance")) || [];
+     document.getElementById("balance").innerHTML=arr[arr.length-1];
     if(arr.length==0){
         var arr = []
         arr.push(totalBalance);
         localStorage.setItem("userBalance", JSON.stringify(arr));
+       
     }
     console.log(arr);
+
+    
 
     $("#btnDeposit").click(function () {
                 var arr = JSON.parse(localStorage.getItem("userBalance")) || [];
@@ -17,9 +20,11 @@ $(document).ready(function () {
                 var amount = Number(userDeposit.val())
                 lastAmount  +=  amount;
                 arr.push(lastAmount);
+                document.getElementById("balance").innerHTML=arr[arr.length-1];
                 localStorage.clear();
                 localStorage.setItem("userBalance", JSON.stringify(arr))
-                console.log(arr);                        
+                console.log(arr); 
+
     });
 
     $("#btnWithdraw").click(function () {
@@ -28,8 +33,10 @@ $(document).ready(function () {
                 var amount = Number(userDeposit.val())
                 lastAmount  -=  amount;
                 arr.push(lastAmount);
+                document.getElementById("balance").innerHTML=arr[arr.length-1];
                 localStorage.clear();
                 localStorage.setItem("userBalance", JSON.stringify(arr))
                 console.log(arr);      
             });
         })
+       
